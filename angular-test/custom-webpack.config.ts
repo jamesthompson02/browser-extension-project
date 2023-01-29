0477b546e497b4fdd558cd7f6923bf845440a997
jamesthompson02 <jamesthompsongithub@outlook.com>
2023-01-29 17:01:11 +0000

    updated a load of files to integrate a service worker into the browser extension. For now, the browser icon gets disabled when logging onto a url that contains google. Updated manifest.json file to add the tabs permission

--- angular-test/custom-webpack.config.ts
+++ angular-test/custom-webpack.config.ts
@@ -1,5 +1,8 @@
 import type { Configuration } from 'webpack';
 
 module.exports = {
-  entry: { 'content': { import: 'src/content.ts', runtime: false } },
+  entry: 
+  { 'content': { import: 'src/content.ts', runtime: false },
+    'background': { import: 'src/background.ts', runtime: false}       
+  },
 } as Configuration;
